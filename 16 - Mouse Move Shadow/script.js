b0fa4dd236ebe05e3e144ce7f1754f400e778c55
unknown <georgesroberto21@gmail.com>
2023-11-20 06:45:23 +0300

    Linked external JavaScript

--- 16 - Mouse Move Shadow/script.js
+++ 16 - Mouse Move Shadow/script.js
@@ -5,5 +5,8 @@ const text = hero.querySelector('h1');
 function shadow(e) {
   const { offsetWidth: width, offsetHeight: height } = hero;
   let { offsetX: x, offsetY: y } = e;
-    console.log(e);
+  console.log(e, y);
+  
 }
+
+hero.addEventListener('mousemove', shadow);
