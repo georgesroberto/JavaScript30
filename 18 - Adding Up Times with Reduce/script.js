33c40b4a49c054c97a4ce78020e409cb7350e698
unknown <georgesroberto21@gmail.com>
2023-11-22 11:23:49 +0300

    Index Page

---
+++ 18 - Adding Up Times with Reduce/script.js
@@ -0,0 +1,20 @@
+const timeNodes = [ ...document.querySelectorAll('[data-time]')];
+
+const seconds = timeNodes
+    .map(node => node.dataset.time)
+    .map(timeCode => {
+        const [mins, secs] = timeCode.split(':').map(parseFloat);
+        return(mins * 60) + secs;
+    })
+    .reduce((total, vidseconds) => total + vidseconds);
+
+
+let secondsLeft = seconds;
+const hours = Math.floor(secondsLeft / 3600);
+secondsLeft = secondsLeft % 3600;
+
+const minutes = Math.floor(secondsLeft / 60);
+secondsLeft = secondsLeft % 60;
+
+console.log(hours, minutes, secondsLeft);
+
