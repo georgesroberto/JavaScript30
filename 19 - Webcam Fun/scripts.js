6217aa79a5ae99aa0398f8bbcfee8101cd6c0911
unknown <georgesroberto21@gmail.com>
2023-11-22 12:53:51 +0300

    Functionality

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,3 +3,91 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false })
+        .then(localMediaStream => {
+            console.log(localMediaStream);
+            video.srcObject = localMediaStream; // Use srcObject instead of createObjectURL
+            video.play();
+        })
+        .catch(error => {
+            console.error('Error accessing webcam:', error);
+        });
+}
+
+function paintToCanvas(){
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0);
+        let pixels = ctx.getImageData(0,0, width, height);
+        pixels = greenScreen(pixels);
+        ctx.globalAlpha = 0.5;
+        ctx.putImageData(pixels, 0, 0);
+        
+      }, 16);
+}
+
+function redEffect(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4 ){
+        pixels.data[i + 0] = pixels.data[i + 0] + 200;
+        pixels.data[i + 1] = pixels.data[i + 1] - 50;
+        pixels.data[i + 2] = pixels.data[i + 2] * 0.5;
+    }
+    return pixels;
+}
+
+function greenScreen(pixels){
+    const levels = [];
+    
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+    for(i = 0; i < pixels.data.length; i+=4){
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+    }
+
+    if(red >= levels.rmin &&
+        green >= levels.gmin &&
+        blue >= levels.bmin &&
+        red <= levels.rmax &&
+        green <= levels.gmax &&
+        blue <= levels.bmax ){
+            pixels.data[i + 3] = 0;
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels){
+    for(let i = 0; i < pixels.data.length; i+=4 ){
+        pixels.data[i - 150] = pixels.data[i + 0];
+        pixels.data[i + 500] = pixels.data[i + 1];
+        pixels.data[i - 550] = pixels.data[i + 2];
+    }
+    return pixels;
+}
+
+function takePhoto(){
+    snap.currenTime = 0;
+    snap.play();
+
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement(`a`);
+    link.href = data;
+    link.setAttribute('download', 'Handsome');
+    link.innerHTML = `<img src="${data}" >`;
+    strip.insertBefore(link, strip.firstChild);
+}
+
+getVideo();
+
+video.addEventListener('canplay', paintToCanvas);
