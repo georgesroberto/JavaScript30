782e8b467f3e6a00a854a9174f7cd798f1c10a2b
unknown <georgesroberto21@gmail.com>
2023-11-24 13:48:59 +0300

    Mouse Events

---
+++ 27 - Click and Drag/script.js
@@ -0,0 +1,31 @@
+const sliders = document.querySelector('.items');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+
+
+sliders.addEventListener('mousedown', (e) => {
+    isDown = true;
+    sliders.classList.add('active');
+    startX = e.pageX - sliders.offsetLeft;
+    scrollLeft = sliders.scrollLeft;
+});
+
+sliders.addEventListener('mouseleave', () => {
+    isDown = false
+    sliders.classList.remove('active');
+});
+
+sliders.addEventListener('mouseup', () => {
+    isDown = false
+    sliders.classList.remove('active');
+});
+
+sliders.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX -sliders.offsetLeft;
+    const walk = (x - startX) * 5;
+    sliders.scrollLeft = scrollLeft - walk;
+});
